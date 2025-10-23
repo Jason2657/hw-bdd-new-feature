b0169dd27a97854f84b61547078e6809c0ae15e8
jax2001 Jason Xie <jax2001@columbia.edu>
2025-10-23 19:29:23 +0000

    CompletedPart 1 and 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,9 @@
-class Movie < ApplicationRecord
+class Movie < ActiveRecord::Base
   #### Part 1 ####
   # implement this method. Remeber to exclude [self]
   # (the current movie) from your return value
   def others_by_same_director
     # Your code here #
+    Movie.where(director: self.director).where.not(id: self.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # Add new routes here
-
+  get 'movies/:id/similar', to: 'movies#similar', as: 'similar'
+  # Add new routes here
   root to: redirect('/movies')
 end
